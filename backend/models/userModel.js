a2f3ac5d61e9f990f04ff851c2cdfe2f953da5b2
LeoC <leo.cheng92@gmail.com>
2022-10-12 01:37:22 -0400

    add groups array to user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,6 +19,12 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true
     },
+    groups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Group'
+      }
+    ]
   },
   {
     timestamps: true,
