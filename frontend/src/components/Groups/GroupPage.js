2629b978748c671811985ab2eabca83538a7640d
LeoC <leo.cheng92@gmail.com>
2022-10-26 11:13:34 -0400

    small adjustment to group page

--- frontend/src/components/Groups/GroupPage.js
+++ frontend/src/components/Groups/GroupPage.js
@@ -3,6 +3,9 @@ import { useParams, useNavigate } from "react-router-dom";
 import { getGroup } from "../../utils/apiHelper";
 import Loading from "../Loading";
 
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
+
 const GroupPage = () => {
   const { path } = useParams();
 
@@ -25,7 +28,22 @@ const GroupPage = () => {
     }
   }, [path, navigate]);
 
-  return <>{loading ? <Loading /> : <h2>{group.title}</h2>}</>;
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Box>
+          <Typography gutterBottom variant="h2">
+            {group.title}
+          </Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {group.description}
+          </Typography>   
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default GroupPage;
