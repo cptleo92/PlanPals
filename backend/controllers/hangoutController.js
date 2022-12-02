aeb77e9ba2b05dd20e1929d19399de21b5d274ee
LeoC <leo.cheng92@gmail.com>
2022-12-02 15:55:27 -0500

    start building hangouts controller. need to refactor frontend to provide the group tied to the hangout

--- backend/controllers/hangoutController.js
+++ backend/controllers/hangoutController.js
@@ -1,10 +1,32 @@
 const Hangout = require('../models/hangoutModel')
+const User = require('../models/userModel')
 
-const getMyHangouts = async (request, response) => {}
+const getMyHangouts = async (request, response) => {
+  const currentUser = await User.findById(request.user.id)
+  response.json(currentUser.hangouts)
+}
 
 const kickFromHangout = async (request, response) => {}
 
-const createHangout = async (request, response) => {}
+const createHangout = async (request, response) => {
+  const { title, description, location, dateOptions } = request.body
+
+  if (!title || !description) {
+    return response.status(400).json({ error: 'All fields are required!' })
+  }
+
+  const currentUser = await User.findById(request.user.id)
+
+  const newHangout = new Hangout({
+    title,
+    description,
+    location,
+    planner: currentUser.id,
+    // group: ,
+    dateOptions,
+    attendees: [currentUser.id],
+  })
+}
 
 const joinHangout = async (request, response) => {}
 
