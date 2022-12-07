66d50b1fb27ea3a735bea5f11fe30cd2efe53dad
LeoC <leo.cheng92@gmail.com>
2022-12-07 14:21:10 -0500

    build back component that navigates to a link prop

---
+++ frontend/src/components/BackArrow.js
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const BackArrow = ({ link }) => {
+  const navigate = useNavigate()
+
+  const handleGoBack = () => {
+    navigate(link)
+  }   
+
+  return (
+      <ArrowBackIcon onClick={handleGoBack} sx={{ cursor: 'pointer' }} />
+  )
+}
+
+export default BackArrow
