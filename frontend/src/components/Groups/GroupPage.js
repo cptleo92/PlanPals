bc10ebc035087f1f188a8261744146339d352856
LeoC <leo.cheng92@gmail.com>
2022-12-20 17:46:56 -0500

    refactor: skeleton-ify GroupPage

--- frontend/src/components/Groups/GroupPage.js
+++ frontend/src/components/Groups/GroupPage.js
@@ -2,16 +2,17 @@ import { useParams, useNavigate, Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { getGroup, joinGroup } from '../../utils/apiHelper'
 import { useCurrentUser } from '../../utils/hooks'
+import placeholder from '../../assets/Placeholder_view_vector.svg'
 
 import AvatarStack from '../Misc/AvatarStack'
 import GroupHangouts from './GroupHangouts'
-import placeholder from '../../assets/Placeholder_view_vector.svg'
+import GroupPageSkeleton from './GroupPageSkeleton'
 
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import BackArrow from '../Misc/BackArrow'
 import Button from '@mui/material/Button'
-import { Container } from '@mui/material'
+import Container from '@mui/material/Container'
 
 
 const GroupPage = () => {
@@ -19,23 +20,18 @@ const GroupPage = () => {
   const { groupPath } = useParams()
   const navigate = useNavigate()
 
+  const test = true
+
   const {
     isLoading,
     error,
     data: group,
   } = useQuery(['group', groupPath], () => getGroup(groupPath))
 
-  if (isLoading) {
-    return
-  }
-
   if (error) {
     navigate('/error')
     console.log(error)
   }
-
-  const hangouts = group.hangouts || []
-  const members = group.members || []
 
   const handleJoin = async () => {
     try {
@@ -55,7 +51,7 @@ const GroupPage = () => {
 
     if (isMemberOrPlanner()) {
       return (
-        <GroupHangouts hangouts={hangouts} />
+        <GroupHangouts hangouts={group.hangouts} />
       )
     } else {
       return (
@@ -72,55 +68,59 @@ const GroupPage = () => {
   }
 
   return (
-    <Box mt={3}>
-      <BackArrow link={'/home'} />
-      <Typography variant="h3" component="h2" mt={3} mb={6}>
-        {group?.title}
-      </Typography>
-
-      <Box
-        component="img"
-        sx={{
-          height: 250,
-          maxHeight: { xs: 150, md: 200, lg: 250 },
-        }}
-        alt="group avatar"
-        src={ group.avatar || placeholder}
-      />
-      <br />
-      {user._id === group.admin._id && (
-        <Link
-          to="./edit"
-          style={{
-            color: 'blue',
-            fontWeight: 500,
-            textDecoration: 'underline',
-          }}
-        >
-          Edit group details
-        </Link>
-      )}
-
-      <Typography gutterBottom variant="h6" mt={4}>
-        Description
-      </Typography>
-      <Typography gutterBottom variant="subtitle1" mt={3}>
-        {group?.description}
-      </Typography>
-
-      <Container sx={{ marginLeft: 0 }} disableGutters maxWidth="sm">
-        <Typography gutterBottom variant="h5" mt={6} mb={3}>
-        Members ({members.length + 1})
+    isLoading
+      ?
+      <GroupPageSkeleton />
+      :
+      <Box mt={3}>
+        <BackArrow link={'/home'} />
+        <Typography variant="h3" component="h2" mt={3} mb={6}>
+          {group?.title}
         </Typography>
 
-        <AvatarStack
-          peopleList={group.members}
-          admin={group.admin}
+        <Box
+          component="img"
+          sx={{
+            height: 250,
+            maxHeight: { xs: 150, md: 200, lg: 250 },
+          }}
+          alt="group avatar"
+          src={ group.avatar || placeholder}
         />
+        <br />
+        {user._id === group.admin._id && (
+          <Link
+            to="./edit"
+            style={{
+              color: 'blue',
+              fontWeight: 500,
+              textDecoration: 'underline',
+            }}
+          >
+          Edit group details
+          </Link>
+        )}
 
-        {renderInfo()}
-      </Container>
-    </Box>
+        <Typography gutterBottom variant="h6" mt={4}>
+        Description
+        </Typography>
+        <Typography gutterBottom variant="subtitle1" mt={3}>
+          {group?.description}
+        </Typography>
+
+        <Container sx={{ marginLeft: 0 }} disableGutters maxWidth="sm">
+          <Typography gutterBottom variant="h5" mt={6} mb={3}>
+        Members ({group.members.length + 1})
+          </Typography>
+
+          <AvatarStack
+            peopleList={group.members}
+            admin={group.admin}
+          />
+
+          {renderInfo()}
+        </Container>
+      </Box>
   )
 }
 
