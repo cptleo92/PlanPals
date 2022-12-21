f67735bb5b11166d33d7ceb80c935037e987183e
LeoC <leo.cheng92@gmail.com>
2022-12-21 16:15:28 -0500

    test: set up basic login test, scripts for faster db resetting

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 } else if (process.env.NODE_ENV === 'development') {
   MONGODB_URI = process.env.DEV_MONGODB_URI
-} else {
+} else if (process.env.NODE_ENV === 'production') {
   MONGODB_URI = process.env.MONGODB_URI
 }
 
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,58 @@
+describe('The Home Page', () => {
+  it('successfully navigates to the login page', () => {
+    cy.visit('/')
+    
+    cy.contains('Register').click()
+
+    cy.url().should('contain', '/register')
+  })
+})
+
+describe('Creating a new user', () => {
+  it('fails if fields are invalid', () => {
+    cy.visit('/register')
+
+    cy.get('input[name=name]').type('Cypress')
+    cy.get('button[type=submit]').click()
+    
+    cy.contains('Email address is required')
+    
+    cy.get('input[name=email]').type('cypress@test.com')
+    cy.get('button[type=submit]').click()
+    
+    cy.contains('Password is required')
+
+    cy.get('input[name=password]').type('pass')
+    cy.get('button[type=submit]').click()
+    
+    cy.contains('Passwords do not match')
+    cy.contains('Password must be at least 6 characters')   
+  })
+
+  it('navigates to the user home page on successful registration', () => {
+    cy.visit('/register')
+
+    cy.get('input[name=name]').type('Cypress')
+    cy.get('input[name=email]').type('cypress@test.com')      
+    cy.get('input[name=password]').clear().type('password')
+    cy.get('input[name=confirmPassword]').clear().type('password')
+
+    cy.get('button[type=submit]').click()
+    
+    cy.url().should('contain', '/home')   
+  })
+
+  it('fails if the user already exists', () => {
+    cy.visit('/register')
+
+    cy.get('input[name=name]').type('Cypress')
+    cy.get('input[name=email]').type('cypress@test.com')      
+    cy.get('input[name=password]').clear().type('password')
+    cy.get('input[name=confirmPassword]').clear().type('password')
+
+    cy.get('button[type=submit]').click()
+
+    cy.contains('User already exists!')
+  })
+})
+
