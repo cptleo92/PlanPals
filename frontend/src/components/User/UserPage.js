c7fd390fd7d2c4eb9987bb2984ccb3507f9e3738
LeoC <leo.cheng92@gmail.com>
2023-01-14 19:00:32 -0500

    feat: getting started on user page

--- frontend/src/components/User/UserPage.js
+++ frontend/src/components/User/UserPage.js
@@ -1,12 +1,16 @@
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography'
 
 const UserPage = () => {
   return (
     <Container maxWidth="md">
-      <Box mt={5}>
+      <Box mt={8} sx={{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}>
         <Avatar sx={{
           width: 250,
           height: 250,
